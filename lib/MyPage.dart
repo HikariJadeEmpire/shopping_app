f3b22d73338d59a6f71a7493b8ffd5ef838d6e1a
HikariJadeEmpire <rastuniing@hotmail.com>
2025-06-19 15:34:33 +0700

    Initial commit

---
+++ lib/MyPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingApp extends StatefulWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  State<ShoppingApp> createState() => _ShoppingAppState();
+}
+
+class _ShoppingAppState extends State<ShoppingApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
